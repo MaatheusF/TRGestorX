09c88edb72d40bfa3e603d827d7cdb1c09c2b0a6
mathe <mathe@DESKTOP-CCOH7NU>
2023-02-20 05:36:49 -0300

    Adicionado estrturua da tela de cadastro de chamados

--- chamados.php
+++ chamados.php
@@ -13,6 +13,12 @@ include("./php/Chamados_ListaChamados.php");
         <link href="css/chamados.css" rel="stylesheet">
         <link href="css/topbar.css" rel="stylesheet">
         <script src="js/chamados-buscar.js"></script>
+   
+   
+   
+   
+   
+   
     </head>
 
 
@@ -58,10 +64,133 @@ include("./php/Chamados_ListaChamados.php");
         <!-- Container-->
         <div class="container2">
 
-            <!-- Botão Flutuante -->
+
+
+            
+            <!-- Botão Flutuante nvo chamado -->
             <div class="flutu-novo">
-                <button class="btn-flutu-novo"><i class="bi bi-plus-circle btn-flutu-icon"></i></button>
+                <button type="button" class="btn  btn-flutu-novo" data-bs-toggle="modal" data-bs-target="#modal-novo-chamado"><i class="bi bi-plus-circle btn-flutu-icon"></i></button>
             </div>
+
+            <!-- Criar novo Chamado -->
+            <!-- Modal -->
+            <div class="modal fade" id="modal-novo-chamado" tabindex="-1" data-bs-backdrop="static" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-xl" style="width: 900px;">
+                    <div class="modal-content">
+                        <div class="modal-body">
+                            <h5 id="novo-chamado-titulo">Criar novo Chamado</h5>
+                            <form>
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="form-floating">
+                                            <select class="form-select form-select-sm shadow-none form-border" required data-select-search="true" id="select-prioridade">
+                                              <option value="1">Baixa</option>
+                                              <option value="2">Media</option>
+                                              <option value="3">Alta</option>
+                                              <option value="3">Critica</option>
+                                            </select>
+                                            <label for="select-prioridade" style="font-size: 13px;">Prioridades</label>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="form-floating mb-3">
+                                            <input type="date" class="form-control shadow-none select-font form-border" id="select-data" placeholder="Dia/Mes/Ano">
+                                            <label for="select-data" style="font-size: 13px;">Data</label>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="form-floating">
+                                            <select class="form-select form-outline form-select-sm shadow-none select-font form-border" required id="select-cliente">
+                                              <option value="1">Gsat</option>
+                                              <option value="2">Upper</option>
+                                              <option value="3">Apisul</option>
+                                              <option value="3">CargoPolo</option>
+                                            </select>
+                                            <label for="select-cliente" style="font-size: 13px;">Cliente</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control shadow-none select-font form-border" id="select-ticket" placeholder="Chamado">
+                                            <label for="select-ticket" style="font-size: 13px;">Ticket</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control shadow-none select-font form-border" required id="select-titulo" placeholder="Titulo">
+                                            <label for="select-titulo" style="font-size: 13px;">Titulo</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-2">
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control shadow-none select-font form-border" id="select-tarefa" placeholder="Tarefa">
+                                            <label for="select-tarefa" style="font-size: 13px;">Cod. Tarefa</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="form-floating">
+                                            <select class="form-select form-outline form-select-sm shadow-none select-font form-border" id="select-fila">
+                                              <option value="1">Em Atendimento</option>
+                                              <option value="2">Analise</option>
+                                              <option value="3">Projeto</option>
+                                              <option value="3">Infraestrutura</option>
+                                            </select>
+                                            <label for="select-fila" style="font-size: 13px;">Fila</label>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="form-floating">
+                                            <select class="form-select form-outline form-select-sm shadow-none select-font form-border" id="select-tipo">
+                                              <option value="1">Duvida</option>
+                                              <option value="2">Defeito</option>
+                                              <option value="3">Outros</option>
+                                            </select>
+                                            <label for="select-tipo" style="font-size: 13px;">Tipo</label>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="form-floating">
+                                            <select class="form-select form-outline form-select-sm shadow-none select-font form-border" id="select-Status">
+                                              <option value="1">Novo</option>
+                                              <option value="2">Aguardando Cliente</option>
+                                              <option value="3">Pendente</option>
+                                              <option value="3">Em Analise</option>
+                                              <option value="3">Em Tente</option>
+                                              <option value="3">Aguardando ATT</option>
+                                              <option value="3">Concluido</option>
+                                            </select>
+                                            <label for="select-Status" style="font-size: 13px;">Status</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="form-floating" style="margin-top: 20px;">                            
+                                            <textarea class="form-control shadow-none select-font form-border" id="select-observacoes" rows="3"></textarea>
+                                            <label for="select-observacoes" style="font-size: 13px;">Observações</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="modal-btn-box">
+                                    <button type="button" class="btn btn-outline-primary btn-md" data-bs-dismiss="modal" style="width: 120px; margin-right: 30px;">Fechar</button>
+                                    <button type="button" class="btn btn-primary btn-md" style="width: 120px;">Finalizar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
 
             <!-- Cabeçalho do Filtro -->
             <div id="filtro-box">
@@ -75,6 +204,9 @@ include("./php/Chamados_ListaChamados.php");
                     </form>   
                 </div>
             </div>
+
+
+
 
             <!-- Tabela -->
             <table class="table" id="tabela-box">
@@ -129,7 +261,11 @@ include("./php/Chamados_ListaChamados.php");
                 </tbody>
             </table>
         </div>
-
-        
+        <!-- jQuery -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <!-- Popper Js -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <!-- Bootstrap JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </body>
 </html>
